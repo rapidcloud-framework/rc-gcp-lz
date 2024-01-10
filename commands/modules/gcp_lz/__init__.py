6fc8b9ee033d9919d19591e8226124423cc04732
Igor Royzis <iroyzis@kinect-consulting.com>
2024-01-10 09:09:50 -0500

    first commit

---
+++ commands/modules/gcp_lz/__init__.py
@@ -0,0 +1,64 @@
+__author__ = "iroyzis@kinect-consulting.com"
+
+from commands.kc_metadata_manager.gcp_metadata import Metadata
+
+class ModuleMetadata(Metadata):
+
+    def __init__(self, args):
+        super().__init__(args)
+        self.args = args
+
+        # gcp sdk
+        # self.boto3_session = super().get_boto3_session()
+
+        # use gcp sdk as follows
+        # s3_client = self.boto3_session.client("s3")
+        # dynamodb_res = self.boto3_session.resource("dynamodb")
+
+
+    def bootstrap_landing_zone(self, metadata=None):
+        # TODO call your shell script
+        # Step 1
+        '''
+        Delete existing `gcp_infra` items.
+        Module automation metadata is stored in your CosmosDB `gcp_infra` table
+        delete existing gcp_infra items for your module instance
+        '''
+        super().delete_infra_metadata(name=self.args.name)
+
+        # Step 2
+        '''
+        Construct `params` dict for each GCP resource you plan to create for this module.
+        Params will be used by terraform modules to generate your infrastructure
+        '''
+        # example:
+        params = {
+            "resource_name": "some_name",
+            "category": "testing",
+            "size": 5
+        }
+
+        # Step 3
+        '''
+        Create `gcp_infra` item for each GCP resource you plan to automate
+        '''
+        # example:
+
+        # must be a valid Terraform supported GCP resource type
+        resource_type = "some_resource_type"
+
+        # must be a unique resource name for the specified resource_type
+        resource_name = "some_name"
+        # TODO uncomment if needed
+        # super().add_infra_resource(resource_type, resource_name, params)
+
+        # TODO
+        '''
+        Repeat steps 2 and 3 for each resource to be generated for this module
+        '''
+
+        # TODO
+        '''
+        Optionally and in addition to creating resources, you can run any code here 
+        to support this module functionality.
+        '''
